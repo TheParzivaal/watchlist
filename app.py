b752f7a3b5c29c43fbaa0d4a8c56189eb5e2f67b
Parzivaal <maxpalmer718@gmail.com>
2023-06-07 15:02:42 +0800

    Add template to render index index page

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 import json
 
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-
-@app.route('/test')
-def test_url_for():
-    res = []
-    res.append(url_for('hello'))
-    res.append(url_for('user_page', name='max'))
-    res.append(url_for('test_url_for'))
-    return json.dumps(res)
+name = "Max Duan"
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
